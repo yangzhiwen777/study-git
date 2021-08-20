b38b7de501f526e3a0474379126d1573ac9bf5d1
金轮 <jinlun.yang@tuya.com>
2021-08-20 14:55:17 +0800

    增加购物车按钮

--- src/shop/index.js
+++ src/shop/index.js
@@ -3,9 +3,19 @@ const Shop = () => {
   useEffect(() => {
     console.log("Registere");
   }, []);
+  const btnClick = () => {
+    console.log("加入购物车按钮");
+  };
   return (
     <>
       <h1>购物页面</h1>
+      <button
+        onClick={() => {
+          btnClick();
+        }}
+      >
+        加入购物车按钮
+      </button>
     </>
   );
 };
