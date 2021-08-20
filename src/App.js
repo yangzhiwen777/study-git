c06406457ccc858e457307e4d6df286127b6a2ed
金轮 <jinlun.yang@tuya.com>
2021-08-20 11:50:31 +0800

    完成了注册功能

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-
-import './App.css';
+import React from "react";
+import Registere from "./registered";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Registere></Registere>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
---
+++ src/registered/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Registere = () => {
+  return (
+    <>
+      <h1>注册</h1>
+    </>
+  );
+};
+export default Registere;
