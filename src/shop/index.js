52a476621715e79c477dcba0fcd9e932dfbcb948
yangzhiwen777 <65332557+yangzhiwen777@users.noreply.github.com>
2021-08-20 14:51:12 +0800

    修改shop结构与生命周期

--- src/shop/index.js
+++ src/shop/index.js
@@ -3,8 +3,12 @@ const Shop = () => {
   useEffect(() => {
     console.log("Registere");
   }, []);
+  useEffect(() => {
+    console.log("Registere-seconed");
+  }, []);
   return (
     <>
+      <h1>购物页面</h1>
       <h1>购物页面</h1>
     </>
   );
